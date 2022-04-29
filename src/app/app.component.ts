5b5246c6627c5822c2f63a9696c2c2ba4f57fa38
Mike Jerred <mjerred.work@gmail.com>
2022-04-29 11:27:07 +0100

    feat: favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
-import { Title } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer, Title } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
 
@@ -16,11 +17,15 @@ export class AppComponent extends BaseComponent {
   xsQuery = this.media.asObservable().pipe(map(changes => changes.some(change => change.mqAlias === 'xs' && change.matches)));
 
   constructor(
+    sanitizer: DomSanitizer,
+    iconRegistry: MatIconRegistry,
     private media: MediaObserver,
     router: Router,
     titleService: Title
   ) {
     super();
+
+    iconRegistry.addSvgIcon('glint', sanitizer.bypassSecurityTrustResourceUrl('/assets/glint.svg'));
 
     router.events
       .pipe(filter(event => event instanceof NavigationEnd))
