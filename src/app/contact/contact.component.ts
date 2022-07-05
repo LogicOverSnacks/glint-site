07d569e127cd68a27b1985fae1df6eae4c9192ae
Mike Jerred <mjerred.work@gmail.com>
2022-07-05 12:02:54 +0100

    feat: add gitter link

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -20,6 +20,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         margin-top: 20px;
       }
 
+      li {
+        line-height: 25px;
+      }
+
+      .banner {
+        height: 20px;
+        display: inline-block;
+        vertical-align: text-top;
+      }
+
       a {
         color: mat.get-color-from-palette(theme.$app-primary-palette, 300);
       }
@@ -30,13 +40,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     <h1 class="title" ngClass.lt-sm="mat-display-1" ngClass.gt-xs="mat-display-2">Contact</h1>
 
     <h2>Sales Enquiries</h2>
+    <ul>
+      <li>
     Email: <a href="mailto:sales@glint.info">sales@glint.info</a>
+      </li>
+    </ul>
 
     <h2>Legal Questions</h2>
-    Email: <a href="mailto:legal@glint.info">legal@glint.info</a>
+    <ul>
+      <li>
+        Email: <a href="mailto:legal@glint.info">legal@glint.info</a>
+      </li>
+    </ul>
 
     <h2>General Help</h2>
-    Email: <a href="mailto:help@glint.info">help@glint.info</a> for anything else.
+    <ul>
+      <li>
+        Gitter: <a class="banner" target="_blank" href="https://gitter.im/git-glint/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"><img src="https://badges.gitter.im/git-glint/community.svg"></a>
+      </li>
+      <li>
+        Email: <a href="mailto:help@glint.info">help@glint.info</a>
+      </li>
+    </ul>
   </app-container>
   `
 })
